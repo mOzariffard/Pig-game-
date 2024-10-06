c8fe69c526d6d8a24382fa59a4f7cd51c7d7d8db
Mohammadreza Zarif <131007236+mOzariffard@users.noreply.github.com>
2024-10-06 19:04:16 +0330

    Update script.js updete the number untill the 100

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ btnHold.addEventListener("click", function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     // 2. check if player's score is >=100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 100) {
       playing = false;
       diceEl.classList.add("hidden");
       // Finish the Game
